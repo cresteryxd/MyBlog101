6757acb34fba2907dd7ac9137c09a57482c01ee7
Xianda <cresteryxd@gmail.com>
2016-10-24 22:25:21 -0400

    Version_1.0

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -20,6 +20,7 @@ class AuthController extends Controller
     | a simple trait to add these behaviors. Why don't you explore it?
     |
     */
+    /*try something for git*/
 
     use AuthenticatesAndRegistersUsers, ThrottlesLogins;
 
